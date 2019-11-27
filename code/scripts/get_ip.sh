cfc4cd68768ab5df360754c239c84a173e046216
cjdcordeiro <cristovao.cordeiro@sixsq.com>
2019-11-27 16:52:20 +0100

    run up script in the background

--- code/scripts/get_ip.sh
+++ code/scripts/get_ip.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
 source /opt/nuvlabox/.env
-
-timestamp_nb_conf=$(stat -c %Y ${VPN_CONF})
-
 # the openvpn client automatically passes the VPN IP into this script
 echo $4 > ${VPN_IP}
 
-# keep watching for updates on the NB VPN configuration file
-timestamp_nb_updated=$timestamp_nb_conf
-while [[ "$timestamp_nb_updated" -le "$timestamp_nb_conf" ]]
-do
-    timestamp_nb_updated=$(stat -c %Y ${VPN_CONF})
-    sleep 1
-done
+BASEDIR=$(dirname "$0")
 
-# if we got here, then the nb.conf was updated and we must restart the openvpn-client
-killall openvpn
+${BASEDIR}/wait-for-vpn-update.sh &
